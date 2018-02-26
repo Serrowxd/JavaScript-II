81f4cd18f676511627310985a107242c893197a4
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-02-26 02:41:05 -0600

    [WIP] JS-II

--- src/recursion.js
+++ src/recursion.js
@@ -3,17 +3,24 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
+
+console.log(nFibonacci(8));
+// note that finding the 35th+ number in the sequence takes a very long time, while looping is much faster.
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
 };
 
 /* eslint-enable no-unused-vars */
